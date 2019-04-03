38b08c10108df0654b121213d9304e42851d5f2b
10sr <8.slashes@gmail.com>
2019-04-03 19:53:36 +0900

    Update b source

--- b_pelican/pelicanconf.py
+++ b_pelican/pelicanconf.py
@@ -38,7 +38,7 @@ RELATIVE_URLS = True
 ##########
 ### Themes
 
-THEME = 'hss'
+THEME = 'theme'
 #USER_LOGO_URL = 'https://s.gravatar.com/avatar/a5f8f466a1c07ac174d899690fdfcbe7?s=80'
 USER_LOGO_URL = ''
 CUSTOM_HEADER_HTML = '''
