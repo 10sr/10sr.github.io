6a7836e701bff0781394a5c8a4cba8b73d5f3ebc
10sr <8.slashes@gmail.com>
2019-04-03 22:18:20 +0900

    Remvoe some confs

--- b_pelican/pelicanconf.py
+++ b_pelican/pelicanconf.py
@@ -39,12 +39,12 @@ RELATIVE_URLS = True
 ### Themes
 
 THEME = 'theme'
-#USER_LOGO_URL = 'https://s.gravatar.com/avatar/a5f8f466a1c07ac174d899690fdfcbe7?s=80'
-USER_LOGO_URL = ''
-CUSTOM_HEADER_HTML = '''
-<style>
-.author, .author__logo {
-    display: none;
-}
-</style>
-'''
+# #USER_LOGO_URL = 'https://s.gravatar.com/avatar/a5f8f466a1c07ac174d899690fdfcbe7?s=80'
+# USER_LOGO_URL = ''
+# CUSTOM_HEADER_HTML = '''
+# <style>
+# .author, .author__logo {
+#     display: none;
+# }
+# </style>
+# '''
