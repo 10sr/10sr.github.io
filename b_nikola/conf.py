e0fd4b25e524cde00c391034828f46e933535677
10sr <8.slashes@gmail.com>
2019-04-05 18:32:47 +0900

    Update BASe_URL

--- b_nikola/conf.py
+++ b_nikola/conf.py
@@ -23,7 +23,7 @@ BLOG_TITLE = "10sr.github.io/b_testing"  # (translatable)
 SITE_URL = "https://10sr.github.io/b_testing/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
-# BASE_URL = "https://10sr.github.io/b_testing/"
+BASE_URL = "/b_testing/"
 BLOG_EMAIL = "8.slashes@gmail.com"
 BLOG_DESCRIPTION = "10sr public Nikola."  # (translatable)
 
