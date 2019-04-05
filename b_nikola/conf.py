81d93b8debcd13d42bab00e08eaf3fbb12cfcf2a
10sr <8.slashes@gmail.com>
2019-04-05 18:44:37 +0900

    Update

--- b_nikola/conf.py
+++ b_nikola/conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = ""  # (translatable)
 BLOG_TITLE = "10sr.github.io/b_testing"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://10sr.github.io/b_testing/"
+SITE_URL = "https://10sr.github.io/b_testing"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = ""
