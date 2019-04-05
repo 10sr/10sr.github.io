3ef973926fcf2926473557975b05a224c11ef336
10sr <8.slashes@gmail.com>
2019-04-05 15:36:21 +0900

    Remove author

--- b_nikola/conf.py
+++ b_nikola/conf.py
@@ -16,7 +16,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "10sr"  # (translatable)
+BLOG_AUTHOR = ""  # (translatable)
 BLOG_TITLE = "My Nikola Site"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
