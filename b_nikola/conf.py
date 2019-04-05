45842f72e5d15742293b6dce66c4a12639dcb01e
10sr <8.slashes@gmail.com>
2019-04-05 19:31:06 +0900

    Update

--- b_nikola/conf.py
+++ b_nikola/conf.py
@@ -138,7 +138,7 @@ TRANSLATIONS_PATTERN = "{path}.{lang}.{ext}"
 
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
-        ("../", "^ Up"),
+        ("//10sr.github.io", "^ Up"),
         ("/", "Top"),
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
