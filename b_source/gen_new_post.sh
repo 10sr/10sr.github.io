3e852ab712f8e350b7e30f0a1d4240228e9ff152
10sr <8.slashes@gmail.com>
2021-03-31 12:45:55 +0900

    Add gen_new_post

---
+++ b_source/gen_new_post.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+read -p "post slug: " slug
+echo slug: $slug
+
+yyyymmdd=`date '+%Y%m%d'`
+date=`date '+%Y-%m-%d %H:%M:%S%z' | sed 's/..$/:&/'`
+
+posts_dir=posts
+
+post_path=$posts_dir/$yyyymmdd-$slug.md
+
+if test -f $post_path
+then
+    echo $post_path already exists.
+    exit 1
+fi
+
+cat <<__EOC__ >$post_path
+---
+title: $slug
+date: $date
+---
+
+Write your post here.
+__EOC__
+
+echo $post_path created
